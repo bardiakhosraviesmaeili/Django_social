243a5551b76fce860ea7df3ceb9734250a1f6393
Bardia Khosravi <bardiakhosraviesmaeili@gmail.com>
2023-03-27 22:03:18 +0430

    backward relationship

--- social/home/models.py
+++ social/home/models.py
@@ -4,14 +4,14 @@ from django.urls import reverse
 
 
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     body = models.TextField()
     slug = models.SlugField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ('created',)
+        ordering = ('-created',)
 
     def __str__(self):
         return f'{self.slug} - {self.updated}'
