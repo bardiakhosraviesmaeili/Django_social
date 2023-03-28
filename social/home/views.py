c2d821bee9a90bb196a66749ebde62c6a284c632
Bardia Khosravi <bardiakhosraviesmaeili@gmail.com>
2023-03-28 21:57:51 +0430

    adding like

--- social/home/views.py
+++ social/home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
-from .models import Post, Comment
+from .models import Post, Comment, Vote
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from .forms import PostCreateUpdateForm, CommentCreateForm, CommentReplyForm
@@ -25,9 +25,12 @@ class PostDetailView(View):
 
     def get(self, request, *args, **kwargs):
         comments = self.post_intance.pcomments.filter(is_reply=False)
+        can_like = False
+        if request.user.is_authenticated and self.post_intance.user_can_like(request.user):
+            can_like = True
         return render(request, 'home/detail.html',
                       {'post': self.post_intance, 'comments': comments, 'form': self.form_class,
-                       'reply_form': self.form_class_reply})
+                       'reply_form': self.form_class_reply, 'can_like': can_like})
 
     @method_decorator(login_required)
     def post(self, request, *args, **kwargs):
@@ -114,4 +117,16 @@ class PostAddReplyView(LoginRequiredMixin, View):
             reply.is_reply = True
             reply.save()
             messages.success(request, 'your reply submitted successfully', 'success')
+        return redirect('home:post_detail', post.id, post.slug)
+
+
+class PostLikeView(LoginRequiredMixin, View):
+    def get(self, request, post_id):
+        post = get_object_or_404(Post, pk=post_id)
+        like = Vote.objects.filter(post=post, user=request.user)
+        if like.exists():
+            messages.error(request, 'You already liked this post', 'danger')
+        else:
+            Vote.objects.create(post=post, user=request.user)
+            messages.success(request, 'You liked this post', 'success')
         return redirect('home:post_detail', post.id, post.slug)
